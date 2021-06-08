f634feb70fa9343f08a74fdd36cc4aedef9191e3
viraayuoktvn <79135457+viraayuoktvn@users.noreply.github.com>
2021-06-08 23:39:22 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,39 +14,5 @@ namespace Nabung_Yuk
             LogIn login = new LogIn();
             login.loginUser();
         }
-
-        public static double inputTarget()
-        {
-            Console.WriteLine("Masukkan target pengeluaran yang Anda inginkan: ");
-            double targetOutcome = Convert.ToDouble(Console.ReadLine());
-
-            return targetOutcome;
-        }
-
-        private static double getReachTarget()
-        {
-            TotalOutcome outcome = new TotalOutcome();
-            double totalOutcome = outcome.CalculateTotalOutcome();
-            double targetOutcome = inputTarget();
-
-            double reachTarget = (totalOutcome/targetOutcome) * 100;
-
-            return reachTarget;
-        }
-
-        public static void mustBeSaved()
-        {
-            TotalOutcome outcome = new TotalOutcome();
-            double totalOutcome = outcome.CalculateTotalOutcome();
-            double targetOutcome = inputTarget();
-            double reachTarget = getReachTarget();
-
-            double mustBeSaved = totalOutcome - targetOutcome;
-
-            Console.WriteLine("Realisasi target pengeluaranmu bulan ini mencapai persentase " + reachTarget);
-            Console.WriteLine("Kamu harus menghemat pengeluaran di bulan depan sebanyak " + mustBeSaved);
-
-            Console.WriteLine("Semangat menabung!");
-        }
     }
 }
