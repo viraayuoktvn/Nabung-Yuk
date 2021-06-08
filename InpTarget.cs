65318f84c06154e6abb169fd9659f4ca4dad6e76
viraayuoktvn <79135457+viraayuoktvn@users.noreply.github.com>
2021-06-08 23:38:41 +0700

    Add files via upload

---
+++ InpTarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nabung_Yuk
+{
+    public class InpTarget
+    {
+        public double inputTarget()
+        {
+            Console.WriteLine("Masukkan target pengeluaran yang Anda inginkan: ");
+            double targetOutcome = Convert.ToDouble(Console.ReadLine());
+
+            return targetOutcome;
+        }
+    }
+}
