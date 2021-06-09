f2feca9033174016a45c95b5a0b59f4232f1a4f7
viraayuoktvn <79135457+viraayuoktvn@users.noreply.github.com>
2021-06-09 07:28:14 +0700

    Update Outcome.cs

--- Outcome.cs
+++ Outcome.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Nabung_Yuk
 {
     public abstract class Outcome
     {
-        public double nominal { get; set; }
+        private double nominal;
     }
 }
