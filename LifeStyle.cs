375ce74c94adf5864ec1cae2122910c20a695660
viraayuoktvn <79135457+viraayuoktvn@users.noreply.github.com>
2021-06-09 11:11:25 +0700

    Update LifeStyle.cs

--- LifeStyle.cs
+++ LifeStyle.cs
@@ -4,9 +4,7 @@ using System.Collections.Generic;
 namespace Nabung_Yuk
 {
     public class LifeStyle : Outcome
-    {
-        private double nomLS;
-
+    {        
         public double nominalLS()
         {
             Console.WriteLine("Masukkan pengeluaran kategori Life Style : ");
